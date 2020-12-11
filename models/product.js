c5edf05623dbf70cd49dac21d26ce2326039d7ee
MinahilKhalid <71635725+MinahilKhalid@users.noreply.github.com>
2020-12-11 20:22:13 +0500

    commit

---
+++ models/product.js
@@ -0,0 +1,9 @@
+var mongoose=require("mongoose");
+var productSchema =mongoose.Schema({
+    name:String,
+    id:String,
+    duration:String,
+    fee:String,
+});
+const Product=mongoose.model("Product",productSchema);
+module.exports=Product;
